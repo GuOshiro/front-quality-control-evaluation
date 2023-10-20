5bb3b70bf6f7a10b121b8a68aeced2855552dc23
Gustavo Oshiro <gustavoaoshiro@gmail.com>
2023-10-20 12:37:45 -0300

    fix - JSONVIEW component type

--- app/src/components/JSONView.tsx
+++ app/src/components/JSONView.tsx
@@ -1,6 +1,6 @@
 import { JsonView, allExpanded, defaultStyles } from "react-json-view-lite";
 import "react-json-view-lite/dist/index.css";
-export function JSONView(data: Object) {
+export function JSONView({ data }: { data: Object }) {
   return (
     <JsonView
       data={data}
